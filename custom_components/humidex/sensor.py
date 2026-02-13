7c53c746f8ec4ba6915a48d2cd3e8367b6f28a80
geckom <nodgesoft@gmail.com>
2026-02-13 13:16:54 +1000

    Rename comfort entity suffix to Comfort

--- custom_components/humidex/sensor.py
+++ custom_components/humidex/sensor.py
@@ -294,7 +294,7 @@ class HumidexComfortSensor(HumidexBaseSensor):
         super().__init__(hass, entry)
         base_name = _normalized_base_name(entry.title)
         self._attr_unique_id = f"{entry.entry_id}_comfort"
-        self._attr_name = f"{base_name} Comfortable"
+        self._attr_name = f"{base_name} Comfort"
 
     @property
     def icon(self) -> str:
